5c25aef14f7ea0d2f01b01b9d53cf5ccb425f4b7
huhdy32 <mong3268@gmail.com>
2024-05-22 20:46:45 +0900

    #5 feat(ConnectionManager): 사용자의 이름을 바탕으로 서비스에 등록하는 로직 구성

---
+++ server/src/test/java/com/server/edm/service/client/BasicClientManagerTest.java
@@ -0,0 +1,64 @@
+package com.server.edm.service.client;
+
+import com.server.edm.net.*;
+import com.server.edm.service.EdmService;
+import com.server.edm.service.downlaod.DownLoadService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.channels.SocketChannel;
+
+class BasicClientManagerTest {
+
+    private BasicClientManager basicClientManager;
+    private MessageTransferManager messageTransferManager;
+    private Encoder encoder = new UTFEncoder();
+
+    void init(final DataTransferManager dataTransferManager) {
+        this.messageTransferManager = new BasicMessageTransferManager(dataTransferManager, encoder);
+        this.basicClientManager = new BasicClientManager(messageTransferManager);
+    }
+
+    void modifyClientResponse(final String response) {
+        final DataTransferManager dataTransferManager = new DataTransferManager() {
+            @Override
+            public byte[] sendAndRecieve(SocketChannel socketChannel, byte[] data) {
+                return encoder.encode(response);
+            }
+
+            @Override
+            public void send(SocketChannel socketChannel, byte[] data) {
+
+            }
+        };
+        init(dataTransferManager);
+    }
+
+    @Test
+    void duplicateNameInSameServiceTest() throws IOException {
+        final EdmService edmService = new DownLoadService();
+        final String name = "tester";
+        modifyClientResponse(name);
+
+        final SocketChannel socketChannel = SocketChannel.open();
+        basicClientManager.register(edmService, socketChannel);
+        final SocketChannel socketChannel1 = SocketChannel.open();
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> basicClientManager.register(edmService, socketChannel1));
+    }
+
+    @Test
+    void duplicateNameInDifferentServiceTest() throws IOException {
+        final EdmService edmService1 = new DownLoadService();
+        final EdmService edmService2 = new DownLoadService();
+        final String name = "tester";
+        modifyClientResponse(name);
+
+        final SocketChannel socketChannel1 = SocketChannel.open();
+        final SocketChannel socketChannel2 = SocketChannel.open();
+
+        basicClientManager.register(edmService1, socketChannel1);
+        basicClientManager.register(edmService2, socketChannel2);
+    }
+}
