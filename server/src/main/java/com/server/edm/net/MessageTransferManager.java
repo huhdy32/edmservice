be0b479e3a06754b7c053e238b77d4a6cb39009e
huhdy32 <mong3268@gmail.com>
2024-05-22 21:11:23 +0900

    #6 feat(MessageTransferManager): 클라이언트와의 반복적인 상호작용을 담당하는 인터페이스 구성

---
+++ server/src/main/java/com/server/edm/net/MessageTransferManager.java
@@ -0,0 +1,11 @@
+package com.server.edm.net;
+
+import java.nio.channels.SocketChannel;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public interface MessageTransferManager {
+    Optional<String> requestUntil(final SocketChannel socketChannel, final String request, final Predicate<String> predicate);
+    Optional<String> requestUntil(final SocketChannel socketChannel, final byte[] data, final Predicate<String> predicate, final int tryCount);
+}
+
