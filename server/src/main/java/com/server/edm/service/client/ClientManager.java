4b25dab028b857c3e160928ae2bcacaa407eb29a
huhdy32 <mong3268@gmail.com>
2024-05-24 15:03:39 +0900

    #8 feat(BasicClientManager): 특정 서비스와 연결된 클라이언트들 가져오는 로직 구성

--- server/src/main/java/com/server/edm/service/client/ClientManager.java
+++ server/src/main/java/com/server/edm/service/client/ClientManager.java
@@ -2,12 +2,15 @@ package com.server.edm.service.client;
 
 import com.server.edm.service.EdmService;
 
+import java.io.BufferedInputStream;
 import java.nio.channels.SocketChannel;
+import java.util.Set;
 
 /**
  * 서비스에 연결된 모든 사용자를 관리
  */
 public interface ClientManager {
     void register(EdmService edmService, SocketChannel socketChannel);
+    Set<SocketChannel> getChannels(EdmService edmService);
     void drop(SocketChannel socketChannel);
 }
