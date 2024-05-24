571db85576f6f0b73db75d757721a4d64ed739d4
huhdy32 <mong3268@gmail.com>
2024-05-24 15:22:22 +0900

    #8 test(BasicClientManagerTest): 각 서비스에 연결된 적절한 채널을 가져오는지 테스트

--- server/src/test/java/com/server/edm/distribute/StreamConnectionDistributorTest.java
+++ server/src/test/java/com/server/edm/distribute/StreamConnectionDistributorTest.java
@@ -2,20 +2,59 @@ package com.server.edm.distribute;
 
 import com.server.edm.net.MessageTransferManager;
 import com.server.edm.service.EdmService;
+import com.server.edm.service.StreamWebResourceAccessManager;
+import com.server.edm.service.WebResourceAccessManager;
+import com.server.edm.service.distribute.FileDistributingManager;
 import com.server.edm.service.downlaod.DownLoadService;
 import com.server.edm.service.client.ClientManager;
 import com.server.edm.service.stream.StreamingService;
+import com.server.edm.ui.DownloadServiceUI;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.nio.channels.SocketChannel;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Predicate;
 
 class StreamConnectionDistributorTest {
     private StreamConnectionDistributor streamConnectionDistributor;
+    /**
+     * mock
+     */
+    private DownloadServiceUI downloadServiceUI = new DownloadServiceUI() {
+        @Override
+        public String getUrl() {
+            return null;
+        }
+
+        @Override
+        public String getFileName() {
+            return null;
+        }
+    }
+;
+    /**
+     * mock
+     */
+    private FileDistributingManager fileDistributingManager = new FileDistributingManager() {
+        @Override
+        public void distribute(Set<SocketChannel> channels, BufferedInputStream fileInputStream, String fileName) {
+
+        }
+    };
+    /**
+     * mock
+     */
+    private WebResourceAccessManager webResourceAccessManager = new WebResourceAccessManager() {
+        @Override
+        public BufferedInputStream access(String url) {
+            return null;
+        }
+    };
 
     /**
      * Mock
@@ -27,11 +66,16 @@ class StreamConnectionDistributorTest {
         }
 
         @Override
+        public Set<SocketChannel> getChannels(EdmService edmService) {
+            return null;
+        }
+
+        @Override
         public void drop(SocketChannel socketChannel) {
 
         }
     };
-    private EdmService downloadService = new DownLoadService(clientManager);
+    private EdmService downloadService = new DownLoadService(clientManager, webResourceAccessManager, downloadServiceUI, fileDistributingManager);
     private EdmService streamingService = new StreamingService(clientManager);
     /**
      * Mock
@@ -47,7 +91,7 @@ class StreamConnectionDistributorTest {
             }
 
             @Override
-            public Optional<String> requestUntil(SocketChannel socketChannel, byte[] data, Predicate<String> predicate, int tryCount) {
+            public Optional<String> requestUntil(SocketChannel socketChannel, String data, Predicate<String> predicate, int tryCount) {
                 // not used
                 return Optional.of(result);
             }
