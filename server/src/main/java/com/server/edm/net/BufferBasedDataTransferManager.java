88cf9d49334190956b96170b77efaa54260d03f2
huhdy32 <mong3268@gmail.com>
2024-05-24 23:15:47 +0900

    #10 feat(BufferBasedDataTransferManager): nio 버퍼 기반 데이터 전송 로직 구현

---
+++ server/src/main/java/com/server/edm/net/BufferBasedDataTransferManager.java
@@ -0,0 +1,56 @@
+package com.server.edm.net;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BufferBasedDataTransferManager implements DataTransferManager {
+
+    private static final int BUFFER_SIZE = 512;
+
+    @Override
+    public byte[] sendAndRecieve(SocketChannel socketChannel, byte[] data, int byteCount) {
+        sendOnly(socketChannel, data, byteCount);
+        return recieveOnly(socketChannel);
+    }
+
+    @Override
+    public void send(SocketChannel socketChannel, byte[] data, int byteCount) {
+        sendOnly(socketChannel, data, byteCount);
+    }
+
+    private byte[] recieveOnly(final SocketChannel socketChannel) {
+        final ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+        List<Byte> receivedData = new ArrayList<>();
+        try {
+            while (socketChannel.read(buffer) > 0) {
+                buffer.flip();
+                while(buffer.hasRemaining()) {
+                    receivedData.add(buffer.get());
+                }
+                buffer.clear();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return listToArray(receivedData);
+    }
+
+    private byte[] listToArray(final List<Byte> receivedData) {
+        final byte[] resultBuffer = new byte[receivedData.size()];
+        for (int i = 0; i < resultBuffer.length; i++) {
+            resultBuffer[i] = receivedData.get(i);
+        }
+        return resultBuffer;
+    }
+
+    private void sendOnly(final SocketChannel socketChannel, final byte[] data, final int byteCount) {
+        try {
+            socketChannel.write(ByteBuffer.wrap(data, 0, byteCount));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
